5f9d834caecfc996ada411d6f5735b65aa5dd336
Tomisin <tomisinjenrola@gmail.com>
2018-04-21 10:19:34 -0230

    Fiexd web3 setup

--- app/routes/r.js
+++ app/routes/r.js
@@ -1,24 +1,40 @@
+/* web3 */
 import Ember from 'ember';
 
+// import Web3 from 'ember-web3/index.js';
+
 export default Ember.Route.extend({  
-    web3: Ember.inject.service(),
+    // web3: Ember.inject.service(),
 
     model: function(params){
         return Ember.$.getJSON(`https://www.reddit.com/r/${params.subreddit}.json`);
     },
-    setupController: function(controller, model) {
-        // let provider = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"))
-        // let web3Instance = this.get('web3').setup(provider);
-        try {
-            let web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-            web3.eth.getAccounts(function(accounts) {
-                console.log(accounts);
-            });
-        } catch(e) {
-            console.log(e);
-        }
+     setupController: function(controller, model) {
+         controller.set('model', model);
+         var coinbase = web3.eth.coinbase;
+         var originalBalance = web3.eth.getBalance(coinbase).toNumber();
+         controller.set("coinbase",coinbase);
+         controller.set("originalBalance",originalBalance);
+         
+         web3.eth.filter('latest').watch(function() {
+             var currentBalance = web3.eth.getBalance(coinbase).toNumber();
+             controller.set("currentBalance",currentBalance);
+         });    
     },
     afterModel: function(r){
         r.subreddit = r.data.children[0].data.subreddit || '';
     }
 });
+
+
+/*import Ember from 'ember';
+
+import web3 from 'ember-web3/services/web3';
+export default Ember.Route.extend({
+  host:'http://127.0.0.1:8545',
+  web3:Ember.inject.service(),
+  model(){     
+    let web3 = new Web3(new Web3.providers.HttpProvider(host));
+    return web3.version;
+  }
+});*/
