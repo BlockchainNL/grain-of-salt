b2e6fc95cad7da2b9c5b19fb49844a047091efc9
seamus blackmore <seamus.blackmore@fonemed.com>
2018-04-21 19:15:54 -0230

    adf

--- app/controllers/r.js
+++ app/controllers/r.js
@@ -1,33 +1,130 @@
 // app/controllers/r.js
 import Ember from 'ember';
+var denominator = 100;
+
+function perc2color(perc) {
+    var r, g, b = 0;
+    if(perc < 50) {
+        r = 255;
+        g = Math.round(5.1 * perc);
+    }
+    else {
+        g = 255;
+        r = Math.round(510 - 5.10 * perc);
+    }
+    var h = r * 0x10000 + g * 0x100 + b * 0x1;
+    return '#' + ('000000' + h.toString(16)).slice(-6);
+}
 
 export default Ember.Controller.extend({  
-    pictures: function() {
-        let model = this.get('model');
-        let c = model.data.children.filterBy('data.domain','i.imgur.com');
-        c.forEach(function(item){
-            let newURL = item.data.url.replace(/^http:\/\//i, 'https://');
-            let thumbURL = item.data.thumbnail.replace(/^http:\/\//i, 'https://');
-            Ember.set(item,'data.url',newURL);
-            Ember.set(item,'data.thumbnail',thumbURL);
-            if(item.data.thumbnail === 'nsfw'){
-                Ember.set(item,"data.thumbnail",'https://farm3.staticflickr.com/2571/3810679130_fbb7494d7b_t.jpg');
-            }
+    users:Ember.A(),
+    posts:Ember.A(),
+    getUserScore:function(user){
+        var blockscore = this.getBlockScore(user.get("name"));
+        var expire = 1*60*60*1000; // 1 hour;
+        if(blockscore.postscount < user.get("postscount") || blockscore.datetime < (Date.now()-expire) ){
+            var score = this.calculateUserScore(user);
+            this.writeToBlock(score);
+            return score;
+        }else{
+            return blockscore;
+        }
+    },
+    calculateUserScore:function(user){
+        var score= user.get("karma");
+        var postscount = Math.floor((Math.random() * 10) + 1);
+        var datetime = Date.now();
+        return Ember.Object.create({
+            score:score,
+            postscount:postscount,
+            datetime
+        })
+    },
+    writeToBlock(score){
+
+    },
+    getBlockScore:function(username){
+        var score= Math.floor((Math.random() * 100) + 1);
+        var postscount = Math.floor((Math.random() * 10) + 1);
+        var datetime = Date.now();
+        return Ember.Object.create({
+            score:score,
+            postscount:postscount,
+            datetime
         });
-    return c;
-    }.property('model.data.children.[]'),
-    posts: function() {
-        let model = this.get('model');
-        let c = model.data.children;
-        c.forEach(function(item){
-            let newURL = item.data.url.replace(/^http:\/\//i, 'https://');
-            let thumbURL = item.data.thumbnail.replace(/^http:\/\//i, 'https://');
-            Ember.set(item,'data.url',newURL);
-            Ember.set(item,'data.thumbnail',thumbURL);
-            if(item.data.thumbnail === 'nsfw' || Ember.isEmpty(item.data.thumbnail)){
-                Ember.set(item,"data.thumbnail",'https://farm3.staticflickr.com/2571/3810679130_fbb7494d7b_t.jpg');
+    },
+    addUser:function(obj){
+        var existingUser = this.getUser(obj.get("id"));
+        if(Ember.isEmpty(existingUser)){
+            obj.set("blockscore", this.getUserScore(obj));
+            var score = obj.get("blockscore.score");
+            var percent = 1 - (score/denominator)
+            percent= percent*100;
+            var color=perc2color(percent);
+            obj.set("color", color)
+            this.get("users").addObject(obj);
+
+        }
+    },
+    addPost:function(obj){
+        if(!Ember.isEmpty(obj)){
+            var existingPost = this.getPost(obj.get("id"));
+            if(Ember.isEmpty(existingPost)){
+                this.get("posts").addObject(obj);
+            }    
+        }else{
+            var x = 3;
+        }
+        
+    },
+    // addUserToPost:function(post){
+    //     var users = this.get("users");
+    //     for(var i = 0; i < users; i++){
+    //         var user = users[i];
+    //         if(user.name === post.name){
+    //             post.user=user;
+    //             return;
+    //         }
+    //     }
+    //     return null;
+    // },
+    getUser:function(id){
+        return this.getObj(id,"user");
+    },
+    getPost:function(id){
+        return this.getObj(id,"post");
+    },
+    getObj:function(id,type){
+        var objs = this.get(type+"s");
+        for(var i = 0; i < objs.length; i++){
+            var obj = objs[i];
+            if(obj.get("id") === id){
+                return obj;
             }
-        });
-    return c;
-    }.property('model.data.children.[]')
+        }
+        return null;
+    },
+
+    // postsRows: function() {
+    //     let model = this.get('model');
+
+    //     if(!Ember.isEmpty(model)){
+    //         let c = model.data.children;
+    //         c.forEach(function(item){
+    //             if(!Ember.isEmpty(item.data)){
+
+
+    //                 let newURL = item.data.url.replace(/^http:\/\//i, 'https://');
+    //                 let thumbURL = item.data.thumbnail.replace(/^http:\/\//i, 'https://');
+    //                 Ember.set(item,'data.url',newURL);
+    //                 Ember.set(item,'data.thumbnail',thumbURL);
+    //                 if(item.data.thumbnail === 'nsfw' || Ember.isEmpty(item.data.thumbnail)){
+    //                     Ember.set(item,"data.thumbnail",'https://farm3.staticflickr.com/2571/3810679130_fbb7494d7b_t.jpg');
+    //                 }
+    //             }
+    //         });
+        
+    //     return c;
+    //     }
+    // }.property('posts.[],posts.@each.user.name')
 });
--- app/routes/r.js
+++ app/routes/r.js
@@ -1,29 +1,218 @@
-/* web3 */
+/* globals web3 */
 import Ember from 'ember';
 
 // import Web3 from 'ember-web3/index.js';
+const dataStore = window.localStorage;
+// const clientID = '_rmWOCAnXKxWYg'; //IMPORTANT - THIS NEEDS TO BE YOUR CLIENT ID FROM YOUR APP
+const clientID = '123iPsWHl3DFqw'; //IMPORTANT - THIS NEEDS TO BE YOUR CLIENT ID FROM YOUR APP
 
+
+const subreddits = ['android'];
+
+function authenticateReddit(){
+
+}
+
+function init() {
+ if (!dataStore.getItem('deviceID')) {
+ dataStore.setItem('deviceID', getRandomID());
+ console.log('Created new deivce ID');
+ }
+ //grabStories();
+}
+
+function getRandomID() {
+ return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+ let r = Math.random() * 16 | 0,
+ v = c == 'x' ? r : (r & 0x3 | 0x8);
+ return v.toString(16);
+ });
+}
+
+function grabStories() {
+ if (!validToken()) {
+ getAccessToken(function(){});
+ } else {
+ for (const i of subreddits) {
+ query('r',i,'new', queryCallback);
+ }
+ }
+}
+
+
+function grabUser(username,callback) {
+ if (!validToken()) {
+ getAccessToken(function(
+    ){
+    grabUser(username,callback);
+    });
+ } else {
+    query('user',username,'about', callback);
+ 
+ }
+}
+
+
+function validToken() {
+ if (!dataStore.getItem('accessToken')) {
+ return false;
+ }
+
+ const currentDate = new Date();
+ const expires = new Date(dataStore.getItem('expires'));
+ const difference = (expires.getTime() - currentDate.getTime());
+ const minutesDifference = Math.ceil(difference / (1000 * 60)); // minutes difference
+ if (minutesDifference < 5) {
+ return false;
+ }
+
+return true;
+}
+
+
+function getAccessToken(rerun) {
+ $.ajax({
+ type: 'post',
+ url: 'https://www.reddit.com/api/v1/access_token',
+ dataType: 'json',
+ headers: {
+ Authorization: `Basic ${btoa(`${clientID}:` + '')}`,
+ },
+ data: { grant_type: 'https://oauth.reddit.com/grants/installed_client', device_id: dataStore.getItem('deviceID') },
+ success(data) {
+     if (data.access_token) {
+         dataStore.setItem('accessToken', data.access_token);
+         dataStore.setItem('expires', new Date().addHours((data.expires_in) / 3600));
+         rerun();
+     }
+ },
+ error(err) {
+ console.log(err);
+ },
+ });
+}
+
+Date.prototype.addHours = function (h) {
+
+this.setTime(this.getTime() + (h*60*60*1000));
+
+return this;
+
+};
+
+/**
+ * Queries the reddit api for a specific subreddit
+ * @param {* string - subrredit name} subreddit 
+ * @param {*function - callback function} callback 
+ */
+function query(parm,val,subquery, callback) {
+    if(Ember.isEmpty(subquery)){
+        var url = `https://oauth.reddit.com/${parm}/${val}`;       
+    }else{
+        var url = `https://oauth.reddit.com/${parm}/${val}/${subquery}`;    
+   }
+ $.ajax({
+ type: 'get',
+ url,
+ dataType: 'json',
+ headers: {
+ Authorization: `Bearer ${dataStore.getItem('accessToken')}`,
+ },
+ success(data) {
+ callback(data);
+ },
+ error(err) {
+ console.log(err);
+ },
+ });
+}
+
+/**
+ * Write the data to page
+ * @param {*} data - json object
+ */
+function queryCallback(data) {
+ // const list = document.createElement('ul');
+ // const posts = data.data.children;
+ // posts.forEach((element) => {
+ // const item = document.createElement('li');
+ // item.innerHTML = element.data.title;
+ // list.appendChild(item);
+ // });
+ // document.body.appendChild(list);
+}
+
+function redditPromise(parm,val){
+    return  new Ember.RSVP.Promise(function(resolve, reject){
+          // succeed
+            $.getJSON('https://www.reddit.com/'+parm+'/'+val+'.json', function(fbResults) {
+ 
+                 resolve(fbResults);
+            });
+          
+          // or reject
+        
+        });
+
+}
 export default Ember.Route.extend({  
     // web3: Ember.inject.service(),
 
     model: function(params){
-        return Ember.$.getJSON(`https://www.reddit.com/r/${params.subreddit}.json`);
-    },
-     setupController: function(controller, model) {
-         controller.set('model', model);
-         var coinbase = web3.eth.coinbase;
-         var originalBalance = web3.eth.getBalance(coinbase).toNumber();
-         controller.set("coinbase",coinbase);
-         controller.set("originalBalance",originalBalance);
+        init();
+        
+
          
-         web3.eth.filter('latest').watch(function() {
-             var currentBalance = web3.eth.getBalance(coinbase).toNumber();
-             controller.set("currentBalance",currentBalance);
-         });    
+    },
+     setupController: function(controller, model, obj) {
+        redditPromise('r',obj.params.r.subreddit).then(function(r){
+            controller.set("model",r);
+            r.subreddit = r.data.children[0].data.subreddit || '';
+            var posts = controller.get("posts");
+            var users = controller.get("users");
+            var route = this;
+            if(!Ember.isEmpty(r.data) && !Ember.isEmpty(r.data.children)){
+                for(var i =0; i < r.data.children.length; i++){
+                    if(!Ember.isEmpty(r.data.children[i].data)){
+                        var post = r.data.children[i].data;
+
+                        if(!Ember.isEmpty(post)){
+
+                            var postObj = Ember.Object.create(post);
+
+                            controller.addPost(postObj);
+                            var username = post["author"];
+                            
+                            if(!Ember.isEmpty(username)){
+                                var anonFunc = function(newPostObj){
+
+
+                                    grabUser(username , function(res){
+                                        if(!Ember.isEmpty(res.data)){
+                                            var user = res.data;
+                                            var userObj = Ember.Object.create(user);
+                                             controller.addUser(userObj);
+                                            newPostObj.set("user",userObj);
+                                            // var controller = route.controller;
+                                            // controller.get("users").addObject(user);
+                                        }
+                                    });
+                                }
+                                anonFunc(postObj);
+
+                            }
+                        } 
+                    }
+                    
+                }    
+            }
+
+        });
+  
     },
     afterModel: function(r){
-        r.subreddit = r.data.children[0].data.subreddit || '';
-    }
+
+           }
 });
 
 
